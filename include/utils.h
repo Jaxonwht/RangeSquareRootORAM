7b9d9ae0d0d8ab39c8f66cd7ec04f43cd2e3a5f7
Naya Chen <xc109@duke.edu>
2019-12-03 22:52:23 -0500

    Add sha256 for crypto hash

--- include/utils.h
+++ include/utils.h
@@ -6,6 +6,6 @@ int encrypt(const void *data, const int size, void *buf);
 
 int decrypt(const void *data, const int size, void *buf);
 
-void *crypto_hash(const int idx, const int key);
+void *crypto_hash(int idx, const int key);
 
 #endif
