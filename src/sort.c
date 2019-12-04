66067cbe798816affe6f8b77077ff91b369a990f
yunhao-qing <qingyunhao@gmail.com>
2019-12-03 23:34:13 -0500

    sort 2

--- src/sort.c
+++ src/sort.c
@@ -6,9 +6,6 @@
 #include <math.h>
 #include <storage.h>
 
-void compare(int i, int j, int dir, *oram, compare);
-void bitonicMerge(int lo, int cnt, int dir, *oram, compare);
-void bitonicSort(int lo, int cnt, int dir, *oram, compare);
 
 const int ASCENDING = 1;
 const int DESCENDING = 0;
@@ -25,6 +22,47 @@ const int DESCENDING = 0;
  * @return 0 on success.
  * @return -1 on failure.
  */
+
+void compareAndSwap(int i, int j, int dir, const struct oram *oram, group_comparator compare)
+{
+  struct group_info info_i;
+  struct group_info info_j;
+  int group_size = oram->group_size;
+  int blk_size = oram->blk_size;
+  int myconst = group_size * blk_size + sizeof(struct group_info);
+  int ret_1 = storage_read(oram->dev, i * myconst, sizeof(struct group_info), &info_i);
+  int ret_2 = storage_read(oram->dev, j * myconst, sizeof(struct group_info), &info_j);
+
+  if (dir==compare(&info_i,&info_j)){
+    int ret_3 = storage_write(oram->dev, i * myconst, sizeof(struct group_info), &info_j);
+    int ret_4 = torage_write(oram->dev, j * myconst, sizeof(struct group_info), &info_i);
+  }
+}
+
+void bitonicMerge(int lo, int cnt, int dir, const struct oram *oram, group_comparator compare)
+{
+if (cnt>1)
+ {
+   int k=cnt/2;
+   int i;
+   for (i=lo; i<lo+k; i++)
+    compareAndSwap(i, i+k, dir, oram, compare);
+   bitonicMerge(lo, k, dir, oram, compare);
+   bitonicMerge(lo+k, k, dir, oram, compare);
+ }
+}
+
+void bitonicSort(int lo, int cnt, int dir, const struct oram *oram, group_comparator compare)
+{
+if (cnt>1)
+ {
+   int k=cnt/2;
+   bitonicSort(lo, k, ASCENDING, oram, compare);
+   bitonicSort(lo+k, k, DESCENDING, oram, compare);
+   bitonicMerge(lo, cnt, dir, oram, compare);
+ }
+ }
+
 int oram_sort(const struct oram *oram, group_comparator compare)
 {
     int N = oram->group_count;
@@ -32,46 +70,10 @@ int oram_sort(const struct oram *oram, group_comparator compare)
 	return 0;
 }
 
-void compare(int i, int j, int dir, const struct oram *oram, group_comparator compare)
-{
-  struct group_info info_i;
-  struct group_info info_j;
-  int group_size = oram->group_size;
-  int blk_size = oram->blk_size;
-  int myconst = group_size * blk_size + sizeof(group_info);
-  int ret_1 = storage.read(oram->dev, i * myconst, sizeof(struct group_info), &info_i);
-  int ret_2 = storage.read(oram->dev, j * myconst, sizeof(struct group_info), &info_j);
 
-  if (dir==compare(info_i,info_j)){
-    storage.write(oram->dev, i * myconst, sizeof(struct group_info), &info_j);
-    storage.write(oram->dev, j * myconst, sizeof(struct group_info), &info_i);
-  }
 
-}
 
-void bitonicSort(int lo, int cnt, int dir, const struct oram *oram, group_comparator compare)
-{
-  if (cnt>1)
-    {
-      int k=cnt/2;
-      bitonicSort(lo, k, ASCENDING, oram, compare);
-      bitonicSort(lo+k, k, DESCENDING, oram, compare);
-      bitonicMerge(lo, cnt, dir, oram, compare);
-    }
-}
 
-void bitonicMerge(int lo, int cnt, int dir, const struct oram *oram, group_comparator compare)
-{
-  if (cnt>1)
-    {
-      int k=cnt/2;
-      int i;
-      for (i=lo; i<lo+k; i++)
-	compare(i, i+k, dir, oram, compare);
-      bitonicMerge(lo, k, dir, oram, compare);
-      bitonicMerge(lo+k, k, dir, oram, compare);
-    }
-}
 /*
  * Improved oblivious sort.
  *
