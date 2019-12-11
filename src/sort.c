df68ba1eabb38a0cdc9516b36c691aeea0b4ceb3
yunhao-qing <qingyunhao@gmail.com>
2019-12-10 23:36:10 -0500

    test

--- src/sort.c
+++ src/sort.c
@@ -1,6 +1,7 @@
 #include <oram.h>
 #include <utils.h>
 #include <stdio.h>
+
 #include <stdlib.h>
 #include <string.h>
 #include <stdint.h>
@@ -12,6 +13,8 @@ enum dir {
 	ASCENDING,
 	DESCENDING
 };
+
+int test[8] = {1, 19, 10,5, 8, 17, 9, 6};
 
 static void compareAndSwap(int i, int j, enum dir dir, const struct oram *oram, group_comparator compare)
 {
@@ -147,5 +150,54 @@ int compare_restore(const struct group_info *a, const struct group_info *b)
 	if (a->state == updated && b->state == old) {
 		return -1;
 	}
+	return 0;
+}
+
+
+static void compareAndSwap_test(int i, int j, enum dir dir)
+{
+	int a = test[i];
+	int b = test[j];
+
+	if ((dir == ASCENDING && a>b) || (dir == DESCENDING && a<b)){
+	    test[i] = b;
+	    test[j] = a;
+	}
+}
+
+static void bitonicMerge_test(int lo, int cnt, const enum dir dir)
+{
+	if (cnt > 1) {
+		const int k = cnt >> 1;
+		for (int i= lo; i < lo + k; i++) {
+			compareAndSwap_test(i, i + k, dir);
+		}
+		bitonicMerge_test(lo, k, dir);
+		bitonicMerge_test(lo + k, k, dir);
+	}
+}
+
+static void bitonicSort_test(int lo, int cnt, const enum dir dir)
+{
+	if (cnt > 1) {
+		const int k = cnt >> 2;
+		bitonicSort_test(lo, k, ASCENDING);
+		bitonicSort_test(lo + k, k, DESCENDING);
+		bitonicMerge_test(lo, cnt, dir);
+	}
+}
+
+
+int oram_sort_test()
+{
+    for (int q = 0; q < 8; q++){
+	    printf("%d", test[q]);
+	}
+	bitonicSort_test(0,7,ASCENDING);
+
+	for (int q = 0; q < 8; q++){
+	    printf("%d", test[q]);
+	}
+
 	return 0;
 }
